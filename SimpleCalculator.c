2357605bffe42e57ce889654ce14eb439fea2b24
Christian G <chrisgobin94@gmail.com>
2021-03-09 22:51:57 -0500

    Working Prototype.
    Calculator works, kind of hacky. Needs optimization and fix the use of all those if statements.

--- SimpleCalculator.c
+++ SimpleCalculator.c
@@ -7,21 +7,44 @@
         -Return answer to user and print to console.
         -Take into consideration invalid input.
         -Handle errors + return 0.
-
-
 */
+        
 float SimpleCalculator(float x, float y){
-    float ans;
+    float ans = 0;
+    char operation[9];
     
     
-    return ans;
+    printf("What operation would you like to perform user?: \n");
+    fgets(operation, 9, stdin);
+    
+    //Probably should find a way to reformat to get rid of all these if statements..
+    
+    if(operation[0] == 'a'){
+        printf("%.2f", (x+y));
+        return 1;
+    }
+    if(operation[0] == 's'){
+        printf("%.2f", (x-y));
+        return 1;
+    }
+    if(operation[0] == 'm'){
+        printf("%.2f", (x*y));
+        return 1;
+    }
+    if(operation[0] == 'd'){
+        printf("%.2f", (x/y));
+        return 1;
+    }
+    else {
+        printf("Invalid option. Try again.");
+        return 0;
+    }
 }
 
 
 
 int main() {
     SimpleCalculator(2,6);
-    
     return 0;
     
 }
