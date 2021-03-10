4691c4ebad02204afddc08f45bbd54ae8d53cdab
Christian G <chrisgobin94@gmail.com>
2021-03-09 22:37:42 -0500

    Updated Function Outline
    Wrote proper outline of what function is expected to do.

--- SimpleCalculator.c
+++ SimpleCalculator.c
@@ -1,36 +1,20 @@
-//Calculator adds, subtracts, multiplies, divides.
+#include <stdio.h>
+
+/*
+    SimpleCalculator function:
+        -Takes 2 args and performs 1 of 4 Arethmetic Operations(add,subtract,divide,multiply)
+        -To select operation, user input is required
+        -Return answer to user and print to console.
+        -Take into consideration invalid input.
+        -Handle errors + return 0.
+
+
+*/
 float SimpleCalculator(float x, float y){
     float ans;
-    //Declare var to store operation user wants to be performed
-    char operation[9];
-    // ask user what operation they would like to perform.
-    printf("What operation would you like to perform?\n");
-    scanf("%s", operation);
     
-    //Sanity check what is being store in operation variable after entering "add";
-    if(operation == "add"){
-        ans = x + y;
-        printf("%f", ans);
-        return ans;
-    }
-    if(operation == "subtract"){
-        ans = x - y;
-        printf("%f", ans);
-        return ans;
-    } 
-    if(operation == "multiply"){
-        ans = x * y;
-        printf("%f", ans);
-        return ans;
-    } 
-    if(operation == "divide"){
-        ans = x / y;
-        printf("%f", ans);
-        return ans;
-    } else {
-        printf("%s\n", operation);
-        printf("Try Again\n");
-    }
+    
+    return ans;
 }
 
 
