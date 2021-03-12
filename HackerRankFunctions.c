6030799d793b7240ad56fca811897e739be16145
Christian G <chrisgobin94@gmail.com>
2021-03-11 23:23:07 -0500

    Working on loop problem
    Swap for loop for while loop. Got to figure our why the output is the wrong num, some type of memory error.

--- HackerRankFunctions.c
+++ HackerRankFunctions.c
@@ -6,24 +6,31 @@
 			Write a function that takes 4 int args and returns the largest int.
 */
 
-int greatest_of_four(int a, int b, int c, int d){
+void greatest_of_four(int a, int b, int c, int d){
 	//make an int array to make the data easier to work with.
 	int numbers[4] = {a,b,c,d};
 	//Loop through each element of the array while keeping track of largest data point.
 	//Only switch the max value when new value is larger.
 	int tracker = numbers[0];
-	for(int i = 1; i < sizeof(numbers); i++){
+	int i = 0;
+	while(i <= sizeof(numbers)){
 		if(numbers[i] > tracker){
 			tracker = numbers[i];
-			printf("%i", tracker);
-			return tracker;
-		} else {
-			printf("%i", tracker);
-			return tracker;
 		}
+		i++;
 	}
-	return 0;
+	printf("%i",tracker);
 }
 int main(int argc, char *argv[]) {
 	greatest_of_four(4, 3, 6, 9);
+	
+	
+	
+	/*
+	Test loop through list.
+	int listy[4] = {1,2,3,4};
+	for(int i = 0; i <= 3; i++){
+		printf("%i\n", listy[i]);
+	}
+	*/
 }
