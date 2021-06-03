77fc679536a074b893559c58242215357a5e73b4
Christian G <chrisgobin94@gmail.com>
2021-06-02 23:57:46 -0400

    Update greatestOfFour.c

--- greatestOfFour.c
+++ greatestOfFour.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
 /*
-	Objective:
-			Write a function that takes 4 int args and returns the largest int.
+Add `int max_of_four(int a, int b, int c, int d)` here.
 */
 
-void greatest_of_four(int a, int b, int c, int d){
-	//make an int array to make the data easier to work with.
-	int numbers[4] = {a,b,c,d};
-	//Loop through each element of the array while keeping track of largest data point.
-	//Only switch the max value when new value is larger.
-	int tracker = numbers[0];
-	int i = 0;
-	while(i <= 4){
-		if(numbers[i] > tracker){
-			tracker = numbers[i];
-		}
-		i++;
-	}
-	printf("%i",tracker);
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
 }
 
-//There is no way to get the length of an array in C, instead use the hard coded arr length to iterate.
-int main(int argc, char *argv[]) {
-	greatest_of_four(4, 2, 1, 1);
+int max_of_four(int a, int b, int c , int d){
+    int arg_array[4] = {a, b, c, d};
+    int lowest = a;
+    for(int i = 0; i <= 4; i++){
+        if(lowest < arg_array[i]){
+            lowest = arg_array[i];
+        }
+    }
+    return lowest;
 }
