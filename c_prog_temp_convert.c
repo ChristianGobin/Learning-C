1c6e2711689838056fe6a6373892c9e37a8572a7
Christian G <chrisgobin94@gmail.com>
2021-06-22 08:01:34 -0400

    Finished Program

--- c_prog_temp_convert.c
+++ c_prog_temp_convert.c
@@ -2,20 +2,19 @@
 #include <math.h>
 //Program prints a table of Farenheit to Celsius Conversion as seen in book
 //The C Programming Language by k&r
-
+//Integer arithmetic would truncate the calculation if the decimals were not specified.
 int main()
 {
 	printf("Fahrenheit\tCelsius\n");
 	float fahrenheit;
-	float celsius = ceilf(5/6);
-	float conversion = 9/5;
+	float celsius;
 	printf("%.2f", celsius);
-	/*
+	
 	for(fahrenheit = 0; fahrenheit <= 200; fahrenheit += 20)
 	{
-		celsius = (fahrenheit - 32 * (5/6));
+		celsius = (fahrenheit - 32.0) * 5.0 / 9.0; 
 		printf("%.2f\t%.2f\n", fahrenheit, celsius);
 	}
-	*/
+	
 }
 
