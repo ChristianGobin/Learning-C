71d1ee1071547b0f27e80219a4d38aab05835942
Christian G <chrisgobin94@gmail.com>
2021-06-25 00:08:08 -0400

    Figured out how to return array from function

--- return_array_practice.c
+++ return_array_practice.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
-//Return an array from a function
 
-int* generate_odd_ints();
+//Figure out how to return array values from a function
+
+char* return_name_string();
+int* return_some_int();
 int main()
 {
-	int* odd_arr_address = generate_odd_ints();
-	printf("%p\n", &odd_arr_address);
+	printf("%s\t at location: %p\n", return_name_string(), return_name_string()); //access array values through format specifier
 	
-	printf("%p", *odd_arr_address[1]);
 	
+	int* my_ptr = return_some_int(); // Store Address of int array.
+	for(int x = 0; my_ptr[x] != 0; x++) //Access data of int array and print data.
+	{
+		//print each%dalue of the array and location in memory of that value.
+		printf("%d\t%p\n", my_ptr[x], my_ptr[x]);
+	}
+	return 0;
 }
 
-int* generate_odd_ints()
+char* return_name_string()
 {
-	int odd_array[10];
-	int x = 0;
-	while(x <= 9)
-	{
-		for(int i = 1; i <= 10; i++)
-		{
-			if(i % 2 != 0)
-			{
-				odd_array[x] = i;
-			}
-		}
-		x++;
-	}
-	return odd_array;
+	static char full_name[] = "Christian Gobin"; //static char array keeps value outside scope of function.
+	return full_name;
+}
+
+int* return_some_int()
+{
+	static int nums[] = {2,4,5,6,33,12};
+	return nums;
 }
