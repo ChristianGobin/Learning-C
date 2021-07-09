a3c1c561a553bd322aea3ccea4c0e64e3b4aa7c0
Christian G <chrisgobin94@gmail.com>
2021-07-09 10:44:47 -0400

    Initial commit get_line

---
+++ my_get_line.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+// Create our own get line function
+
+void get_line()
+{
+  // Logic
+  
+}
+int main()
+{
+   printf("Enter a number: ");
+   char x = getchar();
+
+   printf("Your character = %c", x);
+
+}
