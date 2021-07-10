c6ab45de6f977c1d6058600b2bdb842a452e9513
Christian G <chrisgobin94@gmail.com>
2021-07-09 22:40:29 -0400

    finished get_line

--- my_get_line.c
+++ my_get_line.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
-// Create our own get line function
+// Create our own get line function that takes user input and repeats it in console.
 
-void get_line()
+void get_print_line()
 {
-  
+   char x;
+   while(x != EOF || x != 'n')
+   {
+      x = getchar();
+      putchar(x);
+   }
 }
+
 int main()
 {
-   char *x[20];
-   printf("Enter a number: ");
-   while(*x != EOF || '\n')
-   {
-      *x = getchar();
-   }
+   //user enums
+   enum programs {
+      DataScience,
+      EmbeddedSystems,
+      WebDevelopment
+   };
+   char *program_array[3] = {"Data Science", "Embedded Systems", "Web Development"};
    
-   printf("Your character = %c", x);
-
+   printf("My program is: %s\n", program_array[EmbeddedSystems]);
+   
+   // Get and repeat line
+   get_print_line();
+   
+   
 }
