3b20c71afce65e58efa062bc44630fd10667f5f3
Christian G. <chrisgobin94@gmail.com>
2024-05-13 22:09:36 -0400

    started building out program

---
+++ refresh_0/say_hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+char** get_users_name() {
+    printf("What's your name?\n");
+    scanf();
+}
+
+int main() {
+
+}
+
+
+
+// program asks for a users name, and responds with a greeting + users name
